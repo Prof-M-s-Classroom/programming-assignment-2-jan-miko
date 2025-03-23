b53dd06c9f2ab71e2e6512971d388e19f775db7a
jan-miko <bautistajan425@gmail.com>
2025-03-23 14:15:23 -0700

    Attempted to load file, but keep running into error

--- GameDecisionTree.h
+++ GameDecisionTree.h
@@ -13,15 +13,33 @@ class GameDecisionTree {
 private:
     Node<T>* root;
 
+
 public:
     // TODO: Constructor
     GameDecisionTree() : root(nullptr) {}
 
     // TODO: Function to load story data from a text file and build the binary tree
-    void loadStoryFromFile(const std::string& filename, char delimiter){}
+    void loadStoryFromFile(const std::string& filename, char delimiter) {
+        cout << "loadStoryFromFile" << endl;
+        ifstream MyFile(filename);
+        if (!MyFile.is_open()) {
+            cout << "Error: Unable to open file " << filename << endl;
+            return;
+        }
+        cout << "before loop" << endl;
+        string line;
+        while (getline(MyFile, line)) {
+            cout << line << endl;
+            cout << "looping" << endl;
+        }
+        cout << "after loop" << endl;
+        MyFile.close();
+    }
 
     // TODO: Function to start the game and traverse the tree based on user input
-    void playGame(){}
+    void playGame() {
+        cout << "playGame" << endl;
+    }
 };
 
 #endif // GAMEDECISIONTREE_H
